0cc152f20e6b8e339481ba7de1c9818e8ccb9643
Dmitry <froz3n90@gmail.com>
2022-05-28 00:07:50 +0300

    'start'

---
+++ tickets/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class Ticket(models.Model):
+    title = models.CharField(verbose_name='тема', max_length=64)
+    content = models.TextField(verbose_name='Описание', blank=True)
+    author = models.ForeignKey(User, default=User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    time_create = models.DateTimeField(auto_now_add=True)
+    STATUS_CHOISES = (
+        ('not resolved', 'не решен'),
+        ('resolved', 'решен'),
+        ('frozen', 'заморожен'),
+    )
+    status = models.CharField(max_length=50, choices=STATUS_CHOISES, default='not resolved')
+
+    def __str__(self):
+        return self.title
